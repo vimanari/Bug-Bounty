6a8a241299ece1558b42553ce1a3cbbab64339ae
Justin Keating <60345306+vimanari@users.noreply.github.com>
2020-07-13 05:23:06 -0400

    Multi-thread SimpleHTTPServer for Python3

---
+++ p3multithread.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys, os, socket
+from socketserver import ThreadingMixIn
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+HOST = socket.gethostname()
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+	pass
+	
+'''
+This sets the listening port, default set to 8080
+'''
+if sys.argv[1:]:
+	PORT = int(sys.argv[1])
+else:
+	PORT = 8080
+
+'''
+This sets the working directory of the HTTPServer, by default it is set to where the script is executed.
+'''
+if sys.argv[2:]:
+	os.chdir(sys.argv[2])
+	CWD = sys.argv[2]
+else:
+	CWD = os.getcwd()
+
+server = ThreadingSimpleServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler)
+print("Serving HTTP traffic from", CWD, "on", HOST, "using port", PORT)
+try:
+	while 1:
+		sys.stdout.flush()
+		server.handle_request()
+except KeyboardInterrupt:
+	print("\nShutting down server per users request.")
+	
